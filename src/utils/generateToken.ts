bdfe5d1d18c3f8317a0a5721a70b9f3e55382bb0
MohammadSalman1998 <163789436+MohammadSalman1998@users.noreply.github.com>
2024-10-06 01:13:17 +0300

    Update generateToken.ts

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -33,6 +33,7 @@ export function setTokenCookie(jwtPayload: JWTPayload): string {
     secure: process.env.NODE_ENV === "production",
     sameSite: "none",
     path: "/",
+    domain: 'http://localhost:3000',
     maxAge: 60 * 60 * 24 * 30, // 30 days
   });
   return token; // Return the token string, if that's what you want
