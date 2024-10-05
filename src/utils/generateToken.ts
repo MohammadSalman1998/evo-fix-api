1db3589da31d81f028f158b728b2c919b9f098f4
MohammadSalman1998 <163789436+MohammadSalman1998@users.noreply.github.com>
2024-10-06 01:34:32 +0300

    Update generateToken.ts

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -29,7 +29,7 @@ export function generateJWT(jwtPayload: JWTPayload): string {
 export function setTokenCookie(jwtPayload: JWTPayload): string {
   const token = generateJWT(jwtPayload);
   cookies().set("Token", token, {
-    httpOnly: false,
+    httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "none",
     path: "/",
