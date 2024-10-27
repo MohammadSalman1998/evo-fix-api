32dd19d4527211546aa361168cbc3babfb5397b4
MohammadSalman1998 <mohammad.salman.m1998@gmail.com>
2024-10-27 21:12:18 +0300

    update get users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -25,6 +25,7 @@ export async function GET(request: NextRequest) {
         id: {
           not: adminToken?.id,
         },
+        role:"USER"
       },
       include: {
         customer: true,
