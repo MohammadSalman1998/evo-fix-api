37ddd2bb89f0d51b882959bce81997f642f2cf54
MohammadSalman1998 <mohammad.salman.m1998@gmail.com>
2024-11-05 12:19:17 +0300

    update active

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -160,7 +160,7 @@ export async function PUT(request: NextRequest, { params }: Props) {
         phoneNO: body.phoneNO,
         address: body.address,
         avatar: body.avatar,
-        isActive:  body.isActive || user.isActive,
+        isActive: user.role == Role.ADMIN || user.role == Role.SUBADMIN? body.isActive || user.isActive : user.isActive,
         role: (body.role as Role) || user.role,
         technician:
           user.role === "TECHNICAL"
